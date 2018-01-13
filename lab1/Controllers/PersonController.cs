b15bb3ce699dba9f62848f24d815ab9cb08e1c21
Christian Rhodes <christian.rhodes@oit.edu>
2018-01-13 14:15:12 -0800

    first commit

---
+++ lab1/Controllers/PersonController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab1.Controllers
+{
+    public class PersonController: Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewBag.Message = "My people!";
+            return View();
+        }
+    }
+}
---
+++ lab1/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab1.Models
+{
+    // ========================================= PERSON ========================================= //
+    public class Person
+    {
+        string fname;
+        string lname;
+        DateTime dateOfBirth;
+        char gender;
+
+        public Person() {}
+        public string getfname() { return fname; }
+        public string getlname() { return lname; }
+        public DateTime getdob() { return dateOfBirth; }
+        public char getgender() { return gender; }
+    }
+
+    
+}
